dcddc82243d4bae6f0271a5daad0e9f48e452a5b
SzaffiCat <nakuaat@gmail.com>
2019-02-16 22:59:15 +0100

    in App.java delete magic number

--- src/hu/szaffi/app/App.java
+++ src/hu/szaffi/app/App.java
@@ -3,8 +3,9 @@ package hu.szaffi.app;
 public class App {
     public static void main(String[] args) {
         Zoo zoo = new Zoo();
+        int oneWeek = 7;
         zoo.initAnimals();
-        for (int day = 0; day < 7; day++) {
+        for (int day = 0; day < oneWeek; day++) {
             zoo.simulateADay();
         }
     }
